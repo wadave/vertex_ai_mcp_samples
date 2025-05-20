cfd922fb28f7282517240cefc1fd690ef55e2e63
Dave Wang <wangdave@google.com>
2025-05-20 20:43:09 +0000

    init

---
+++ a2a-mcp-app2/weather_agent_preload/adk_agent.py
@@ -0,0 +1,13 @@
+from google.adk.agents import LlmAgent
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
+
+
+def create_agent(mcp_tools: MCPToolset) -> LlmAgent:
+    """Constructs the ADK agent using a preloaded MCPToolset."""
+    return LlmAgent(
+        model="gemini-2.5-flash-preview-04-17",
+        name="weather_agent",
+        description="An agent that can help questions about weather",
+        instruction=f"""You are a specialized weather forecast assistant. Your primary function is to utilize the provided tools to retrieve and relay weather information in response to user queries. You must rely exclusively on these tools for data and refrain from inventing information. Ensure that all responses include the detailed output from the tools used and are formatted in Markdown""",
+        tools=mcp_tools,
+    )
